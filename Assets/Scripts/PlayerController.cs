32e951df05efc68c4797f17f9e50c847a9dcf3e1
David-Lee Kulsch <The1andONLYdave@googlemail.com>
2014-09-14 19:06:56 +0200

    initial push
    http://www.hummelwalker.de/2013/03/17/2d-jump-n-run-spielersteuerung-unity3d-tutorial-3/
    
    https://psm.playstation.net/static/general/all/unity_for_psm/en/Documentation/Manual/PSMInput.html

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float gravity = 6;
+	public float speed = 10;
+	public float jumpPower = 10;
+
+	bool inputJump = false;
+	float velocity = 0;
+	Vector3 moveDirection = Vector3.zero;
+	CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputCheck();
+		Move();
+		SetAnimation();
+	}
+
+	void InputCheck(){
+		velocity = Input.GetAxis ("Horizontal") * speed;
+
+		if ((Input.GetKeyDown(KeyCode.Space))||(Input.GetKeyDown(KeyCode.JoystickButton0))) { //ps vita x?
+			inputJump=true;
+				}
+		else{
+			inputJump=false;	
+		}
+	}
+	void Move(){
+		if (characterController.isGrounded) {
+			if(inputJump){
+				moveDirection.y=jumpPower;
+			}		
+		}
+		moveDirection.x = velocity;
+		moveDirection.y -= (gravity*Time.deltaTime);
+		characterController.Move (moveDirection * Time.deltaTime);
+	}
+	void SetAnimation(){
+
+	}
+
+}
---
+++ Assets/Scripts/SpriteController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class SpriteController : MonoBehaviour {
+
+	public List<Texture2D> animationStayRight;
+	public List<Texture2D> animationStayLeft;
+	public List<Texture2D> animationGoRight;
+	public List<Texture2D> animationGoLeft;
+	public List<Texture2D> animationJumpRight;
+	public List<Texture2D> animationJumpLeft;
+	public List<Texture2D> animationAttackRight;
+	public List<Texture2D> animationAttackLeft;
+	public float speed = 1;
+	public AnimationType currentAnimationType = AnimationType.stayRight;
+
+	public enum AnimationType{
+		stayRight,
+		stayLeft,
+		goRight,
+		goLeft,
+		jumpRight,
+		jumpLeft,
+		attackRight,
+		attackLeft
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (currentAnimationType) {
+		
+		case AnimationType.stayRight:
+			SetTexture (animationStayRight);
+			break;
+		case AnimationType.stayLeft:
+			SetTexture (animationStayLeft);
+			break;
+		case AnimationType.goRight:
+			SetTexture (animationGoRight);
+			break;
+		case AnimationType.goLeft:
+			SetTexture (animationGoLeft);
+			break;
+		case AnimationType.jumpRight:
+			SetTexture (animationJumpRight);
+			break;
+		case AnimationType.jumpLeft:
+			SetTexture (animationJumpLeft);
+			break;
+		case AnimationType.attackRight:
+			SetTexture (animationAttackRight);
+			break;
+		case AnimationType.attackLeft:
+			SetTexture (animationAttackLeft);
+			break;
+
+		}
+
+	}
+
+	void SetTexture(List<Texture2D> animationSprite){
+
+		int index = (int)(Time.time * speed);
+		index = index % animationSprite.Count;
+		renderer.material.mainTexture = animationSprite [index];
+
+	}
+	public void SetAnimation(AnimationType animationType){
+		currentAnimationType = animationType;
+	}
+}
