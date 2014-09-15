c1a01397a0fa401d0707097df3a4f51d655bf04b
David-Lee Kulsch <The1andONLYdave@googlemail.com>
2014-09-15 12:26:29 +0200

    video 5

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthController : MonoBehaviour {
+
+	public float currentHealth = 5;
+	public float damageEffectPause = 0.2F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ApplyDamage(float damage){
+
+		if(currentHealth>0){
+			currentHealth -= damage;
+	
+			if (currentHealth < 0) {
+				currentHealth = 0;		
+			}
+	
+			if (currentHealth ==0){
+				//gameover
+				RestartScene();
+			}
+			else{
+				StartCoroutine(DamageEffect()); 
+			}
+ 		}
+	}
+
+	void RestartScene(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	IEnumerator DamageEffect(){
+		renderer.enabled = false;
+		yield return new WaitForSeconds(damageEffectPause);
+		renderer.enabled = true;
+		yield return new WaitForSeconds(damageEffectPause);
+		renderer.enabled = false;
+		yield return new WaitForSeconds(damageEffectPause);
+		renderer.enabled = true;
+		yield return new WaitForSeconds(damageEffectPause);
+		renderer.enabled = true;
+	}
+}
---
+++ Assets/Scripts/SendDamageCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendDamageCollider : MonoBehaviour {
+
+	public int damageValue = 1;
+	public string tag = "Player";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other){
+				if (other.gameObject.tag == tag) {
+						//only when object have applydamage, else no error because !requirereceiver
+						other.gameObject.SendMessage ("ApplyDamage", damageValue, SendMessageOptions.DontRequireReceiver);
+				}
+		}
+}
