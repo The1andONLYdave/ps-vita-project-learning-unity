3d89188af5da6e257173e514f141a87ebf778e07
David-Lee Kulsch <The1andONLYdave@googlemail.com>
2014-09-15 15:25:21 +0200

    video 8

--- Assets/Scripts/WaypointWalker.cs
+++ Assets/Scripts/WaypointWalker.cs
@@ -5,12 +5,21 @@ using System.Collections.Generic;
 public class WaypointWalker : MonoBehaviour {
 
 	public float speed=1;
+	public int currentHealth=1;
 	public List<Vector3> waypointPositions;
+	public float damageEffectPause = 0.2F;
+	public string tag = "Player";
+	public int damageValue = 1;
+
+	public GameObject deathPrefabRight;
+	public GameObject deathPrefabLeft;
+
 	int currentWaypoint = 0;
 	Vector3 targetPositionDelta;
 	Vector3 moveDirection = Vector3.zero;
 	SpriteController spriteController;
 	bool lookRight = true;
+	bool isHit = false;
 	// Use this for initialization
 	void Start () {
 		spriteController = GetComponent<SpriteController> ();
@@ -54,6 +63,54 @@ public class WaypointWalker : MonoBehaviour {
 		}
 		else{ 
 			spriteController.SetAnimation(SpriteController.AnimationType.goLeft);
+		}
+	}
+	void ApplyDamage(int damage){
+				if (!isHit) {
+						
+						isHit=true;
+						currentHealth -= damage;
+
+						if (currentHealth <= 0) {
+								currentHealth = 0;		
+								Die ();
+						} else {
+								StartCoroutine (DamageEffect ()); 
+						}
+				}
+
+	}
+
+	IEnumerator DamageEffect(){
+		renderer.enabled = false;
+		yield return new WaitForSeconds(damageEffectPause);
+		renderer.enabled = true;
+		yield return new WaitForSeconds(damageEffectPause);
+		renderer.enabled = false;
+		yield return new WaitForSeconds(damageEffectPause);
+		renderer.enabled = true;
+		yield return new WaitForSeconds(damageEffectPause);
+		renderer.enabled = true;
+		isHit = false;
+	}
+
+	void Die(){
+		if (lookRight) {
+			Destroy(Instantiate (deathPrefabRight,transform.position,Quaternion.identity),5);		
+		} 
+		else {
+			Destroy(Instantiate (deathPrefabLeft,transform.position,Quaternion.identity),5);		
+		}
+
+		Destroy (gameObject);
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if (!isHit) {
+						if (other.gameObject.tag == tag) {
+								//only when object have applydamage, else no error because !requirereceiver
+								other.gameObject.SendMessage ("ApplyDamage", damageValue, SendMessageOptions.DontRequireReceiver);
+						}
 		}
 	}
 }
