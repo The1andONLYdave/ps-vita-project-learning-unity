2e52221f9463281e1fac8e9924e1e6762f5ec65c
David-Lee Kulsch <The1andONLYdave@googlemail.com>
2014-09-16 13:17:04 +0200

    video 10

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,16 @@ public class PlayerController : MonoBehaviour {
 	public float gravity = 6;
 	public float speed = 10;
 	public float jumpPower = 10;
-
+	public SendDamageCollider sendDamageColliderR;
+	public SendDamageCollider sendDamageColliderL;
 	bool inputJump = false;
 	float velocity = 0;
 	Vector3 moveDirection = Vector3.zero;
 	CharacterController characterController;
 	SpriteController spriteController;
 	bool lookRight = true;
-
+	bool isSlaying = false;
+	float slayingTime = 0.2F;
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController> ();
@@ -26,6 +28,8 @@ public class PlayerController : MonoBehaviour {
 		InputCheck();
 		Move();
 		SetAnimation();
+		Fight();
+
 	}
 
 	void InputCheck(){
@@ -38,12 +42,17 @@ public class PlayerController : MonoBehaviour {
 						lookRight = false;
 				}
 
-		if ((Input.GetKeyDown(KeyCode.Space))||(Input.GetKeyDown(KeyCode.JoystickButton0))) { //ps vita x?
+		if ((Input.GetButtonDown ("Jump"))||(Input.GetKeyDown(KeyCode.JoystickButton0))) { //ps vita x?
 			inputJump=true;
 				}
 		else{
 			inputJump=false;	
 		}
+		if (((Input.GetButtonDown ("Fire1")) || (Input.GetKeyDown (KeyCode.JoystickButton2)))&&!isSlaying) { //ps vita square
+			isSlaying=true;		
+			StartCoroutine(ResetIsSlaying());
+		}
+
 	}
 	void Move(){
 		if (characterController.isGrounded) {
@@ -54,6 +63,22 @@ public class PlayerController : MonoBehaviour {
 		moveDirection.x = velocity;
 		moveDirection.y -= (gravity*Time.deltaTime);
 		characterController.Move (moveDirection * Time.deltaTime);
+	}
+	void Fight(){
+		if (isSlaying) {
+			if(lookRight){
+				sendDamageColliderR.attacking=true;
+				sendDamageColliderL.attacking=false;
+			}	
+			else{
+				sendDamageColliderL.attacking=true;
+				sendDamageColliderR.attacking=false;
+			}
+		}
+		else{
+			sendDamageColliderL.attacking=false;
+			sendDamageColliderR.attacking=false;
+		}
 	}
 	void SetAnimation(){
 
@@ -79,6 +104,17 @@ public class PlayerController : MonoBehaviour {
 				spriteController.SetAnimation(SpriteController.AnimationType.jumpLeft);
 			}
 		}
+		if (isSlaying) {
+			if(lookRight){
+				spriteController.SetAnimation (SpriteController.AnimationType.attackRight);
+			}
+			else{
+				spriteController.SetAnimation (SpriteController.AnimationType.attackLeft);
+			}
+		}
 	}
-
+	IEnumerator ResetIsSlaying(){
+		yield return new WaitForSeconds(slayingTime);
+		isSlaying = false;
+	}
 }
--- Assets/Scripts/SendDamageCollider.cs
+++ Assets/Scripts/SendDamageCollider.cs
@@ -5,15 +5,34 @@ public class SendDamageCollider : MonoBehaviour {
 
 	public int damageValue = 1;
 	public string tag = "Player";
+	public bool attacking=false;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	void OnTriggerEnter(Collider other){
+		if(attacking){		
 				if (other.gameObject.tag == tag) {
 						//only when object have applydamage, else no error because !requirereceiver
 						other.gameObject.SendMessage ("ApplyDamage", damageValue, SendMessageOptions.DontRequireReceiver);
 				}
 		}
+	}
+	void OnTriggerStay(Collider other){
+		if(attacking){		
+			if (other.gameObject.tag == tag) {
+				//only when object have applydamage, else no error because !requirereceiver
+				other.gameObject.SendMessage ("ApplyDamage", damageValue, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+	void OnTriggerExit(Collider other){
+		if(attacking){		
+			if (other.gameObject.tag == tag) {
+				//only when object have applydamage, else no error because !requirereceiver
+				other.gameObject.SendMessage ("ApplyDamage", damageValue, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
 }
