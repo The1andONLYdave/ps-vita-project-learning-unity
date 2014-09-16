3ca32b5bbcdfe8613b63646592317d69fa1d6e99
David-Lee Kulsch <The1andONLYdave@googlemail.com>
2014-09-16 14:22:37 +0200

    video 11
    parallax scrolling added and background layer 1 and 2

---
+++ Assets/Scripts/LayerParallax.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerParallax : MonoBehaviour {
+
+	public CharacterController cc;
+	public float speedFactor=1;
+
+	Vector3 velocity;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity.x = cc.velocity.x * speedFactor;
+		transform.Translate (velocity * Time.deltaTime);
+	}
+}
